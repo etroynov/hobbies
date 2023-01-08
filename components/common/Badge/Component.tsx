2873171ede3ee596714cdad95e9ace42d5df8326
Evgenii Troinov <troinof@gmail.com>
2023-01-08 13:22:29 +0100

    feat(main): add classes for badge

---
+++ components/common/Badge/Component.tsx
@@ -0,0 +1,31 @@
+import cx from "classnames";
+import type { FC, ReactNode } from "react";
+
+import styles from "./styles.module.css";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Budge: FC<Props> = ({ children }) => (
+  <>
+    <div className={cx(styles.badge, styles.badge__default)}>
+      {children}
+    </div>
+    <div className={cx(styles.badge, styles.badge__primary)}>
+      {children}
+    </div>
+    <div className={cx(styles.badge, styles.badge__secondary)}>
+      {children}
+    </div>
+    <div className={cx(styles.badge, styles.badge__secondary_2)}>
+      {children}
+    </div>
+    <div className={cx(styles.badge, styles.badge__secondary_3)}>
+      {children}
+    </div>
+    <div className={cx(styles.badge, styles.badge__secondary_3)}>
+      {children}
+    </div>
+  </>
+);
